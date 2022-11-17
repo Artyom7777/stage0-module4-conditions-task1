1b90951cb2d696a41224497c9db2726bc23443c9
Artyom7777 <iartiq@gmail.com>
2022-11-17 15:48:06 +0300

    Task is dove

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int a = 0;
+        boolean b = a !=0;
+        if (b = false) {
+        } else {
+            System.out.println("false");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int a = 6;
+        boolean b = a !=0;
+        if (b = true) {
+            System.out.println("true");
+        }
     }
 }
